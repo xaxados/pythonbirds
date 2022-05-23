dbee0a280e334f4703fc623a9922f059a23b5df0
xaxados <7PCS50beto>
2022-05-23 08:20:59 -0300

    Criados atributos de intância, nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,21 @@
 class Pessoa:
+
+    def __init__(self, nome=None, idade=30):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return(f'Olá {id(self)}')
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('André')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Claudia'
+    print(p.nome)
+    print(p.idade)
+
+
 
